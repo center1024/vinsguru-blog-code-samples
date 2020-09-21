7b50f5fd35a0c74c359ddca5d09c648de2ba15ea
center1024 <yuanxin1024@163.com>
2020-09-21 10:37:29 +0800

    Update MovieService.java
    List.of  --> Array.asList

--- rsocket/spring-rsocket/src/main/java/com/vinsguru/springrsocket/service/MovieService.java
+++ rsocket/spring-rsocket/src/main/java/com/vinsguru/springrsocket/service/MovieService.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Service
 public class MovieService {
 
-    private final List<MovieScene> scenes =  List.of(
+    private final List<MovieScene> scenes =  Arrays.asList(
             new MovieScene(1, "Scene 1"),
             new MovieScene(2, "Scene 2"),
             new MovieScene(3, "Scene 3"),
